836b528b5b1ea650d164b75673c6fca2b89e7c5a
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-03-17 12:56:43 +0100

    Update install-nfs-server.sh

--- install-nfs-server.sh
+++ install-nfs-server.sh
@@ -7,6 +7,6 @@ sudo chmod -R 777 /k8s-storage
 cat ~/k8s-lxd/etc_exports | sudo tee -a /etc/exports >/dev/null 2>&1
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
-sudo ufw allow from 192.168.1.0/24 to any port nfs
-sudo ufw allow from 192.168.1.0/24 to any port ssh
-sudo ufw enable
+# sudo ufw allow from 192.168.1.0/24 to any port nfs
+# sudo ufw allow from 192.168.1.0/24 to any port ssh
+# sudo ufw enable
