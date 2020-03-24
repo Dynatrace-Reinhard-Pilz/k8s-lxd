4f337c3c0ae3ffdcd45b267621ffab43d811cde3
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-03-24 14:28:19 +0100

    Update install-keptn.sh

--- install-keptn.sh
+++ install-keptn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
-git clone https://github.com/keptn/keptn.git
-# export DOMAIN=mushroom.home
+# git clone https://github.com/keptn/keptn.git
+export DOMAIN=mushroom.home
 export PLATFORM=kubernetes
 export ISTIO_INSTALL_OPTION=Reuse
 cd ~/keptn/installer/scripts
