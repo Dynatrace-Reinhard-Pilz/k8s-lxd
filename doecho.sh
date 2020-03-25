c7b8c020f04e983d6e362793170fdd295abc2f18
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-03-25 10:09:39 +0100

    Update doecho.sh

--- doecho.sh
+++ doecho.sh
@@ -2,8 +2,8 @@
 if [ "$(whoami)" = "root" ] ; then
     echo "$(whoami)"
     sudo -H -u ubuntu bash -c 'curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/doecho.sh | sh'
-    exit
+else
+    curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/doecho.sh | sh
 fi
-curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/doecho.sh | sh
 # echo "I am $USER, with uid $UID"
 # sudo -H -u ubuntu bash -c 'echo "I am $USER, with uid $UID"'
