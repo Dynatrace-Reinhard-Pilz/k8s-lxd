2b076cbf86bb2834e6678b113f6092e9cde67fd9
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-03-25 10:12:07 +0100

    kl

--- decho.sh
+++ decho.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$(whoami)" = "root" ] ; then
     echo "$(whoami)"
-    sudo -H -u ubuntu bash -c 'curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/doecho.sh | sh'
+    sudo -H -u ubuntu bash -c 'curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/decho.sh | sh'
 else
     echo "I am $USER, with uid $UID"
 fi
