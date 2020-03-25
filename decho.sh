5467988e38cab35c7e48e6ad741b535d35de7b46
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-03-25 10:14:18 +0100

    Update decho.sh

--- decho.sh
+++ decho.sh
@@ -2,8 +2,8 @@
 if [ "$(whoami)" = "root" ] ; then
     echo "$(whoami)"
     sudo -H -u ubuntu bash -c 'curl https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/decho.sh | sh'
-else
-    echo "I am $USER, with uid $UID"
+    exit 0
 fi
+echo "I am $USER, with uid $UID"
 # 
 # sudo -H -u ubuntu bash -c 'echo "I am $USER, with uid $UID"'
