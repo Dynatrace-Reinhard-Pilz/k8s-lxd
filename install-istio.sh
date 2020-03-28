638768a53618dba51c68878295e04f8c577984f3
dtcookie <41952460+dtcookie@users.noreply.github.com>
2020-03-28 18:57:56 +0100

    Update install-istio.sh

--- install-istio.sh
+++ install-istio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~
 curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.5.0
+cd istio-1.5.1
 export PATH=$PWD/bin:$PATH
 istioctl manifest apply --set profile=demo
 kubectl label namespace default istio-injection=enabled
