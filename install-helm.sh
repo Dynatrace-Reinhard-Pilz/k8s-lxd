74ea344ceb7b3b1979dba45f7d7a2ae711ba9510
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-04-03 17:38:15 +0200

    Update install-helm.sh

--- install-helm.sh
+++ install-helm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "...... curl -o helm-v2.16.1-linux-amd64.tar.gz https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz"
 curl -o helm-v2.16.1-linux-amd64.tar.gz https://get.helm.sh/helm-v2.16.1-linux-amd64.tar.gz 2>&1
-echo "...... tar -zxvf helm-v2.16.1-linux-amd64.tar.gz"
 tar -zxvf helm-v2.16.1-linux-amd64.tar.gz 2>&1
-echo "...... mv linux-amd64/helm /usr/local/bin/helm"
 sudo mv linux-amd64/helm /usr/local/bin/helm 2>&1
-echo "...... mv linux-amd64/tiller /usr/local/bin/tiller"
 sudo mv linux-amd64/tiller /usr/local/bin/tiller 2>&1
