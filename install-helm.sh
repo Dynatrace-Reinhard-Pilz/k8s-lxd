4c1d603f7d2b9d92f86b8aa50ead721cbfbccdba
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2020-04-03 18:31:27 +0200

    Update install-helm.sh

--- install-helm.sh
+++ install-helm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-kubectl -n kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# kubectl -n kube-system create serviceaccount tiller
+# kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+# helm init --service-account tiller
