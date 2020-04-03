bca73267560da59176cfd65e4b5260538bfda96c
dtcookie <41952460+dtcookie@users.noreply.github.com>
2020-04-03 07:45:24 +0200

    Update bootstrap-kube-common.sh

--- bootstrap-kube-common.sh
+++ bootstrap-kube-common.sh
@@ -4,8 +4,8 @@ if [ "$(whoami)" = "root" ] ; then
     sudo -H -u ubuntu bash -c 'curl -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/prepare-kube.sh | sh'
     exit 0
 fi
-sudo rm -Rf /tmp/kube-install >/dev/null 2>&1
-sudo mkdir -p /tmp/kube-install >/dev/null 2>&1
+# sudo rm -Rf /tmp/kube-install >/dev/null 2>&1
+# sudo mkdir -p /tmp/kube-install >/dev/null 2>&1
 
 grep -q "deb https://apt.kubernetes.io/ kubernetes-xenial main" /etc/apt/sources.list.d/kubernetes.list >/dev/null 2>&1
 if [ $? -ne 0 ] ; then
@@ -16,11 +16,11 @@ fi
 sudo apt-get update
 sudo apt-get -yq upgrade
 sudo apt-get -yq autoremove
-sudo apt-get -yq install docker.io apt-transport-https curl jq iptables arptables ebtables linux-modules-5.3.0-42-generic nfs-common
+sudo apt-get -yq install docker.io apt-transport-https curl jq iptables arptables ebtables nfs-common
 
 sudo systemctl enable docker
-sudo sed -i -e 's/containerd.sock/containerd.sock\ --exec-opt\ native.cgroupdriver=systemd/' /lib/systemd/system/docker.service
-sudo systemctl daemon-reload
+# sudo sed -i -e 's/containerd.sock/containerd.sock\ --exec-opt\ native.cgroupdriver=systemd/' /lib/systemd/system/docker.service
+# sudo systemctl daemon-reload
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo usermod -aG docker root
@@ -37,15 +37,15 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable kubelet
 sudo systemctl start kubelet
 
-sudo mknod /dev/kmsg c 1 11
-sudo echo "[Unit]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "After=network.service" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "[Service]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "ExecStart=mknod /dev/kmsg c 1 11" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "[Install]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo echo "WantedBy=default.target" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
-sudo chmod 664 /etc/systemd/system/mknod_kmsg.service
-sudo systemctl daemon-reload
-sudo systemctl enable mknod_kmsg.service
+# sudo mknod /dev/kmsg c 1 11
+# sudo echo "[Unit]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "After=network.service" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "[Service]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "ExecStart=mknod /dev/kmsg c 1 11" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "[Install]" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo echo "WantedBy=default.target" | sudo tee -a /etc/systemd/system/mknod_kmsg.service
+# sudo chmod 664 /etc/systemd/system/mknod_kmsg.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable mknod_kmsg.service
