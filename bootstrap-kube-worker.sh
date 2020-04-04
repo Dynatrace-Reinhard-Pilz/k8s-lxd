e321fc5e438229f62d9f3f1454026a70b6359842
dtcookie <41952460+dtcookie@users.noreply.github.com>
2020-04-04 17:41:59 +0200

    Update bootstrap-kube-worker.sh

--- bootstrap-kube-worker.sh
+++ bootstrap-kube-worker.sh
@@ -12,4 +12,5 @@ do
   sleep 10
   JOINCMD=`rsh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l ubuntu k8s-master kubeadm token create --print-join-command 2>/dev/null`
 done
+echo "sudo $JOINCMD  --ignore-preflight-errors=all"
 echo "sudo $JOINCMD  --ignore-preflight-errors=all" | /bin/sh
