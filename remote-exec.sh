01330a1f9f5b59bb9e43128d4016b3a25418f016
dtcookie <41952460+dtcookie@users.noreply.github.com>
2020-04-05 10:09:09 +0200

    Create remote-exec.sh

---
+++ remote-exec.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ "$(whoami)" = "root" ] ; then
+    echo "$(whoami)"
+    sudo -H -u ubuntu bash -c 'curl -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Dynatrace-Reinhard-Pilz/k8s-lxd/master/remote-exec.sh | bash'
+    exit 0
+fi
